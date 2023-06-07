14afa0106032a03a2acad22b8b66760660f2c6a9
SelmarionX <113622270+SelmarionX@users.noreply.github.com>
2023-06-07 14:56:50 +0300

    Add files via upload

---
+++ seminar6/task2.sql
@@ -0,0 +1,24 @@
+# Задача 2
+-- Выведите только четные числа от 1 до 10. Пример: 2,4,6,8,10
+
+delimiter //
+create function even_numbers ()
+returns varchar(100)
+deterministic
+begin
+declare i int default 1;
+declare result varchar(100) default "";
+	while i <= 10 do
+		if (i % 2 = 0) then
+			set result = concat(result, " ", i);
+			set i = i + 2;
+		else
+			set i = i + 1;
+        end if;
+	end while;
+return result;
+end //
+delimiter ;
+
+-- проверка
+select even_numbers();
